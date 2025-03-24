1310b19a80dbdcdd914bc448f51e76279faada4d
Adekoya Daniel <adekoyadaniel53@gmail.com>
2025-03-24 21:25:02 +0100

    Update yarn.lock to optimize dependency resolutions and ensure compatibility with updated packages; streamline package management for improved project stability.

--- src/components/bits/Marquee.tsx
+++ src/components/bits/Marquee.tsx
@@ -20,7 +20,7 @@ const Marquee = ({
   }, [children]);
 
   return (
-    <div className="relative w-full overflow-hidden text-white py-2 h-fit">
+    <div className="relative w-full overflow-hidden text-white h-[100px] py-2">
       <div
         ref={marqueeRef}
         className={`flex whitespace-nowrap absolute ${
